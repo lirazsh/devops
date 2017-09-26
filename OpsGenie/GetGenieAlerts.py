bd61fa8e1ea93cec0586ab6448ced3b3ee4e1089
root <root@pa-lirazs.kaltura.com>
2017-09-26 10:57:04 -0400

    fixed OpsGenie

--- OpsGenie/GetGenieAlerts.py
+++ OpsGenie/GetGenieAlerts.py
@@ -3,17 +3,20 @@
 import requests
 import json
 
+API_LIMIT = '100'
+API_QLEN = 10000
+
 idList = []
 alertData = []
 _apiKey = 'GenieKey 75d7eadd-c38f-4f37-a6fd-e759dafcd41d'
 
-jdata = json.loads(requests.get('https://api.opsgenie.com/v2/alerts?searchIdentifier=moovingon&searchIdentifierType=name',headers={'Authorization' : _apiKey}).content)
-
-for i in jdata['data']:
-	idList.append(i['id'])
+for API_OFFSET in xrange(0,API_QLEN,100):
+	jdata = json.loads(requests.get('https://api.opsgenie.com/v2/alerts?searchIdentifier=moovingon&searchIdentifierType=name&limit=100&offset=' + str(API_OFFSET)  ,headers={'Authorization' : _apiKey}).content)
+	for i in jdata['data']:
+		idList.append(i['id'])
 
 for i in idList:
 	alertData.append(requests.get('https://api.opsgenie.com/v2/alerts/' + i + '?identifierType=id', headers={'Authorization' : _apiKey}).content)
-
+	
 for i in alertData:
 	print(json.dumps(json.loads(i), indent=4, sort_keys=True))
